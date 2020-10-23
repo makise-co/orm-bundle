c7902b208d2396ba0c95301f215b44e782fde35a
Dmitry K <coder1994@gmail.com>
2020-10-23 16:23:29 +0300

    Lazy ORM initialization to prevent DB connection in non-coroutine context

--- src/ORMProvider.php
+++ src/ORMProvider.php
@@ -15,6 +15,7 @@ use Cycle\ORM;
 use Cycle\Schema;
 use DI\Container;
 use MakiseCo\Config\ConfigRepositoryInterface;
+use MakiseCo\ORM\Console\LazyORM;
 use MakiseCo\Providers\ServiceProviderInterface;
 use Spiral\Database\Config\DatabaseConfig;
 use Spiral\Database\DatabaseManager;
@@ -60,41 +61,45 @@ class ORMProvider implements ServiceProviderInterface
         $container->set(
             ORM\ORM::class,
             static function (Container $container, DatabaseManager $dbal, ConfigRepositoryInterface $config) {
-                // Class locator
-                $cl = (new Tokenizer\Tokenizer(
-                    new Tokenizer\Config\TokenizerConfig(
+                $initializer = static function () use ($container, $dbal, $config): ORM\ORM {
+                    // Class locator
+                    $cl = (new Tokenizer\Tokenizer(
+                        new Tokenizer\Config\TokenizerConfig(
+                            [
+                                'directories' => $config->get('database.orm.entityPath', []),
+                                'exclude' => $config->get('database.orm.entityExclude', [])
+                            ]
+                        )
+                    ))->classLocator();
+
+                    $schema = (new Schema\Compiler())->compile(
+                        new Schema\Registry($dbal),
                         [
-                            'directories' => $config->get('database.orm.entityPath', []),
-                            'exclude' => $config->get('database.orm.entityExclude', [])
+                            new Annotated\Embeddings($cl),            // register annotated embeddings
+                            new Annotated\Entities($cl),              // register annotated entities
+                            new Schema\Generator\ResetTables(),       // re-declared table schemas (remove columns)
+                            new Annotated\MergeColumns(),             // register non field columns (table level)
+                            new Schema\Generator\GenerateRelations(), // generate entity relations
+                            new Schema\Generator\ValidateEntities(),  // make sure all entity schemas are correct
+                            new Schema\Generator\RenderTables(),      // declare table schemas
+                            new Schema\Generator\RenderRelations(),   // declare relation keys and indexes
+                            new Annotated\MergeIndexes(),             // register non entity indexes (table level)
+                            new Schema\Generator\GenerateTypecast(),  // typecast non string columns
                         ]
-                    )
-                ))->classLocator();
+                    );
 
-                $schema = (new Schema\Compiler())->compile(
-                    new Schema\Registry($dbal),
-                    [
-                        new Annotated\Embeddings($cl),            // register annotated embeddings
-                        new Annotated\Entities($cl),              // register annotated entities
-                        new Schema\Generator\ResetTables(),       // re-declared table schemas (remove columns)
-                        new Annotated\MergeColumns(),             // register non field columns (table level)
-                        new Schema\Generator\GenerateRelations(), // generate entity relations
-                        new Schema\Generator\ValidateEntities(),  // make sure all entity schemas are correct
-                        new Schema\Generator\RenderTables(),      // declare table schemas
-                        new Schema\Generator\RenderRelations(),   // declare relation keys and indexes
-                        new Annotated\MergeIndexes(),             // register non entity indexes (table level)
-                        new Schema\Generator\GenerateTypecast(),  // typecast non string columns
-                    ]
-                );
+                    $orm = new ORM\ORM(
+                        new ORM\Factory($dbal, null, null, null),
+                        new ORM\Schema($schema)
+                    );
 
-                $orm = new ORM\ORM(
-                    new ORM\Factory($dbal, null, null, null),
-                    new ORM\Schema($schema)
-                );
+                    $proxyFactory = $container->make(\Cycle\ORM\Promise\ProxyFactory::class);
+                    $orm = $orm->withPromiseFactory($proxyFactory);
 
-                $proxyFactory = $container->make(\Cycle\ORM\Promise\ProxyFactory::class);
-                $orm = $orm->withPromiseFactory($proxyFactory);
+                    return $orm;
+                };
 
-                return $orm;
+                return new LazyORM($initializer);
             }
         );
     }
